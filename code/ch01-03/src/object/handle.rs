3ce5e12629c3d4bf93e099388e9a159df5c3df87
DeathWish5 <zyr_ms@outlook.com>
2021-07-31 16:53:49 +0800

    ch1-3 完成 Channel 基础代码，还需要调整注释。

---
+++ code/ch01-03/src/object/handle.rs
@@ -0,0 +1,18 @@
+// ANCHOR: handle
+use super::{KernelObject, Rights};
+use alloc::sync::Arc;
+
+/// 内核对象句柄
+#[derive(Clone)]
+pub struct Handle {
+    pub object: Arc<dyn KernelObject>,
+    pub rights: Rights,
+}
+
+impl Handle {
+    /// 创建一个新句柄
+    pub fn new(object: Arc<dyn KernelObject>, rights: Rights) -> Self {
+        Handle { object, rights }
+    }
+}
+// ANCHOR_END: handle
